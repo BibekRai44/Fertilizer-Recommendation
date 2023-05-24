93e5690c8397aa604654e82f83b5816f50f5a07d
Bibekkun <bibekrai655@gmail.com>
2023-05-24 19:01:16 +0545

    placeholder changed for every crop

--- fertilizer_recommendation.py
+++ fertilizer_recommendation.py
@@ -2,28 +2,39 @@ import streamlit as st
 
 crops = ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas', 'mothbeans', 'mungbean', 'blackgram', 'lentil', 'pomegranate', 'banana', 'mango', 'grapes', 'watermelon', 'muskmelon', 'apple', 'orange', 'papaya', 'coconut', 'cotton', 'jute', 'coffee']
 
+def get_placeholder(crop):
+    if crop == 'rice':
+        return ("Enter nitrogen value (0-80)","Enter phosphorous value (0-40)",'Enter potassium value (0-40)','Enter soil moisture value (0-30)','Enter pH value (0.0-5.5)')
+    elif crop == 'maize':
+        return ("Enter nitrogen value (0-80)","Enter phosphorous value (0-40)",'Enter potassium value (0-40)','Enter soil moisture value (0-30)','Enter pH value (0.0-5.5)')
+    else:
+        return "Nothing"
+    
 def main():
     st.title("Fertilizer Recommendation System")
-    nitrogen = st.text_input('Nitrogen', placeholder="Enter nitrogen value (20-120)")
-    phosphorus = st.text_input('Phosphorous', placeholder='Enter phosphorous value (10-125)')
-    potassium = st.text_input('Potassium', placeholder='Enter potassium value (10-200)')
-    soil_moisture = st.text_input('Soil Moisture', placeholder='Enter soil moisture value (15-90)')
-    ph = st.text_input('Ph Value', placeholder='Enter pH value (4.0-6.5)')
     crop = st.selectbox('Select Crop', crops)
+    placeholders = get_placeholder(crop)
+    nitrogen = st.text_input('Nitrogen', placeholder=placeholders[0])
+    phosphorus = st.text_input('Phosphorous', placeholder=placeholders[1])
+    potassium = st.text_input('Potassium', placeholder=placeholders[2])
+    soil_moisture = st.text_input('Soil Moisture', placeholder=placeholders[3])
+    ph = st.text_input('Ph Value',placeholder=placeholders[4])
 
     if st.button('Recommend Fertilizer'):
         if crop == 'rice':
-            if 20 <= int(nitrogen) <= 120 and 10 <= int(phosphorus) <= 125 and 10 <= int(potassium) <= 200 and 15 <= int(soil_moisture) <= 90 and 4.0 <= float(ph) <= 6.5:
-                st.success("The best fetilizer to use in rice is Urea. \n\n you can use locally available fertilizer like animal dung to increase land fertility as well as rice production \n\n धानमा प्रयोग गर्ने सबैभन्दा राम्रो मल युरिया हो। यूरिया प्रयोग गर्नुहोस् | \n\n जमिनको उर्वराशक्ति र धान उत्पादन बढाउन स्थानीय स्तरमा उपलब्ध मल जस्तै जनावरको गोबर प्रयोग गर्न सकिन्छ ।")
-                st.image('images/143797_shutterstock_723175018.jpg', caption='Urea', use_column_width=True)
-                st.image('images/istockphoto-684977254-612x612.jpg', caption='Animal dung', use_column_width=True)
+            if 0 <= int(nitrogen) <= 80 and 0 <= int(phosphorus) <= 40 and 0 <= int(potassium) <= 40 and 0 <= int(soil_moisture) <= 30 and 0.0 <= float(ph) <= 5.5:
+                st.success("The best fertilizer to use in rice is Urea. \n\n you can use locally available fertilizer like animal dung to increase land fertility as well as rice production \n\n धानमा प्रयोग गर्ने सबैभन्दा राम्रो मल युरिया हो। यूरिया प्रयोग गर्नुहोस् | \n\n जमिनको उर्वराशक्ति र धान उत्पादन बढाउन स्थानीय स्तरमा उपलब्ध मल जस्तै जनावरको गोबर प्रयोग गर्न सकिन्छ ।")
+                st.image('images/143797_shutterstock_723175018.jpg', caption='Urea (युरिया)', use_column_width=True)
+                st.image('images/istockphoto-684977254-612x612.jpg', caption='Animal dung (जनावरको गोबर)', use_column_width=True)
             else:
-                st.success("You have entered wrong input values for rice. Please check again.")
+                st.success("You have entered wrong input values for rice. Please check again. \n\n तपाईंले गलत इनपुट दिनुभएको छ कृपया राम्रोसँग जाँच गर्नुहोस् र पुन: प्रयास गर्नुहोस्")
         elif crop == 'maize':
-            if 30 <= int(nitrogen) <= 150 and 20 <= int(phosphorus) <= 135 and 15 <= int(potassium) <= 180 and 20 <= int(soil_moisture) <= 80 and 5.5 <= float(ph) <= 7.0:
-                st.success("Recommended Fertilizer for maize: DAP (Diammonium Phosphate)")
+            if 0 <= int(nitrogen) <= 80 and 0 <= int(phosphorus) <= 40 and 0 <= int(potassium) <= 40 and 0 <= int(soil_moisture) <= 50 and 0.0 <= float(ph) <= 5.5:
+                st.success("The best fertilizer to use in maize is NPK 15:15:15 which contains 15% of nitrogen,phosphrous and potassium. \n\n Locally available organic fertilizer like animal dung would be perfect to yield maize production. \n\n मकैमा प्रयोग गर्ने उत्तम मल NPK 15:15:15 हो जसमा 15% नाइट्रोजन, फस्फ्रस र पोटासियम हुन्छ। \n\n स्थानीय रूपमा उपलब्ध जैविक मल जस्तै जनावरको गोबर मकै उत्पादन गर्न उपयुक्त हुनेछ।")
+                st.image('https://saprotan-utama.com/wp-content/uploads/2020/11/15_NPK151515.jpg',caption='NPK 15:15:15')
+                st.image('https://www.wur.nl/upload/5160f541-9f91-44b7-bfbc-08e0f4727505_shutterstock_1284414196.jpg',caption='Animal manure (पशु मल)',use_column_width=True)
             else:
-                st.success("You have entered wrong input values for maize. Please check again.")
+                st.success("You have entered wrong input values for maize. Please check again. \n\n तपाईंले गलत इनपुट दिनुभएको छ कृपया राम्रोसँग जाँच गर्नुहोस् र पुन: प्रयास गर्नुहोस्")
         else:
             st.success("Recommended Fertilizer for {} crop: Urea".format(crop))
 
